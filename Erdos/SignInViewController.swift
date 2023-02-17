3620e40b10637b7a86d58c57afffbfe22a1c2763
ebarnes <ebarnes@caps.ua.edu>
2023-02-17 15:03:52 -0500

    initial commit - remove storyboard

---
+++ Erdos/SignInViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Erdos
+//
+//  Created by Eric Barnes on 2/17/23.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .orange
+    }
+
+
+}
+
